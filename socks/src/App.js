31e32b310d39fab09a28337d885078fbefc7b5fb
Allison Demirok-Winters <allisonwinters.studio@gmail.com>
2024-04-15 13:10:39 -0700

    Basic set up of characters and movement in canvas

--- socks/src/App.js
+++ socks/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from 'react';
+import "./App.css";
 
-function App() {
+const App = () => {
+
+  const canvasRef = useRef(null);
+ 
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+
+    let elon = { x: 375, y: 500, speed: 5 };
+    let byte = { x: 325, y: 500, speed: 4 };
+
+    let keysPressed = {};
+    document.addEventListener('keydown', (event) => {
+      keysPressed[event.key] = true;
+    });
+    document.addEventListener('keyup', (event) => {
+      keysPressed[event.key] = false;
+    });
+
+    const moveElon = () => {
+      if (keysPressed['ArrowUp']) elon.y -= elon.speed;
+      if (keysPressed['ArrowDown']) elon.y += elon.speed;
+      if (keysPressed['ArrowRight']) elon.x += elon.speed;
+      if (keysPressed['ArrowLeft']) elon.x -= elon.speed;
+    };
+    // byte's movement is determined by socks falling, always trying to catch the socks/moving towards the falling socks
+    const drawCharacters = () => {
+      context.fillStyle = 'blue';
+      context.fillRect(elon.x, elon.y, 50, 50);
+
+      context.fillStyle = 'red';
+      context.fillRect(byte.x, byte.y, 30, 30);
+    };
+
+    const updateGame = () => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      moveElon();
+      drawCharacters();
+      requestAnimationFrame(updateGame);
+    };
+
+    updateGame();
+  }, []);
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <canvas ref={canvasRef} width={800} height={600} className='game-canvas'/>
     </div>
-  );
-}
+  )
+};
 
 export default App;
